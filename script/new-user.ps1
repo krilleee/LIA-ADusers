f4e819a9f656c39f1a67fd164c07e51819c9fef8
isse <ismail1ibrahim451@gmail.com>
2026-03-16 15:11:01 +0100

    fixat lite

--- script/new-user.ps1
+++ script/new-user.ps1
@@ -1,2 +1,5 @@
 Write-Host "hey to everyone in the world"
-$Age = Read-Host "Please enter your age"
+$Age = Read-Host "Please enter your age
+write-host "meny"
+Write-Host "1. Ny användare"
+Write-Host "2. Avsluta"
