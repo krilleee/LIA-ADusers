3952b6ca8fa0032aaf5a911ad286dc2d5f0b044c
isse <ismail1ibrahim451@gmail.com>
2026-03-16 15:12:15 +0100

    ta bort gammla saker la till nya föranvändare

--- script/new-user.ps1
+++ script/new-user.ps1
@@ -1,5 +1,8 @@
-Write-Host "hey to everyone in the world"
-$Age = Read-Host "Please enter your age
 write-host "meny"
 Write-Host "1. Ny användare"
 Write-Host "2. Avsluta"
+$choice = Read-Host "Välj ett alternativ"
+
+if ($choice -eq 1) {
+    Write-Host "Vad är ditt namn"
+}
